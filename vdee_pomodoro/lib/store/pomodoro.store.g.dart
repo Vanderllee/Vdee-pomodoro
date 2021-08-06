2b975fd60019e660b546fffd1b62f717a3f3fe9a
Vanderllee <vanderllee@gmail.com>
2021-08-06 18:14:16 -0300

    added methods store for actions on components screen

--- vdee_pomodoro/lib/store/pomodoro.store.g.dart
+++ vdee_pomodoro/lib/store/pomodoro.store.g.dart
@@ -9,10 +9,185 @@ part of 'pomodoro.store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$PomodoroStore on _PomodoroStore, Store {
+  final _$startedAtom = Atom(name: '_PomodoroStore.started');
+
+  @override
+  bool get started {
+    _$startedAtom.reportRead();
+    return super.started;
+  }
+
+  @override
+  set started(bool value) {
+    _$startedAtom.reportWrite(value, super.started, () {
+      super.started = value;
+    });
+  }
+
+  final _$minutesAtom = Atom(name: '_PomodoroStore.minutes');
+
+  @override
+  int get minutes {
+    _$minutesAtom.reportRead();
+    return super.minutes;
+  }
+
+  @override
+  set minutes(int value) {
+    _$minutesAtom.reportWrite(value, super.minutes, () {
+      super.minutes = value;
+    });
+  }
+
+  final _$secondsAtom = Atom(name: '_PomodoroStore.seconds');
+
+  @override
+  int get seconds {
+    _$secondsAtom.reportRead();
+    return super.seconds;
+  }
+
+  @override
+  set seconds(int value) {
+    _$secondsAtom.reportWrite(value, super.seconds, () {
+      super.seconds = value;
+    });
+  }
+
+  final _$workTimeAtom = Atom(name: '_PomodoroStore.workTime');
+
+  @override
+  int get workTime {
+    _$workTimeAtom.reportRead();
+    return super.workTime;
+  }
+
+  @override
+  set workTime(int value) {
+    _$workTimeAtom.reportWrite(value, super.workTime, () {
+      super.workTime = value;
+    });
+  }
+
+  final _$restTimeAtom = Atom(name: '_PomodoroStore.restTime');
+
+  @override
+  int get restTime {
+    _$restTimeAtom.reportRead();
+    return super.restTime;
+  }
+
+  @override
+  set restTime(int value) {
+    _$restTimeAtom.reportWrite(value, super.restTime, () {
+      super.restTime = value;
+    });
+  }
+
+  final _$intervalTypeAtom = Atom(name: '_PomodoroStore.intervalType');
+
+  @override
+  IntervalType get intervalType {
+    _$intervalTypeAtom.reportRead();
+    return super.intervalType;
+  }
+
+  @override
+  set intervalType(IntervalType value) {
+    _$intervalTypeAtom.reportWrite(value, super.intervalType, () {
+      super.intervalType = value;
+    });
+  }
+
+  final _$_PomodoroStoreActionController =
+      ActionController(name: '_PomodoroStore');
+
+  @override
+  void start() {
+    final _$actionInfo = _$_PomodoroStoreActionController.startAction(
+        name: '_PomodoroStore.start');
+    try {
+      return super.start();
+    } finally {
+      _$_PomodoroStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void stop() {
+    final _$actionInfo = _$_PomodoroStoreActionController.startAction(
+        name: '_PomodoroStore.stop');
+    try {
+      return super.stop();
+    } finally {
+      _$_PomodoroStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void restart() {
+    final _$actionInfo = _$_PomodoroStoreActionController.startAction(
+        name: '_PomodoroStore.restart');
+    try {
+      return super.restart();
+    } finally {
+      _$_PomodoroStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void workIncrement() {
+    final _$actionInfo = _$_PomodoroStoreActionController.startAction(
+        name: '_PomodoroStore.workIncrement');
+    try {
+      return super.workIncrement();
+    } finally {
+      _$_PomodoroStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void workDecrement() {
+    final _$actionInfo = _$_PomodoroStoreActionController.startAction(
+        name: '_PomodoroStore.workDecrement');
+    try {
+      return super.workDecrement();
+    } finally {
+      _$_PomodoroStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void restIncrement() {
+    final _$actionInfo = _$_PomodoroStoreActionController.startAction(
+        name: '_PomodoroStore.restIncrement');
+    try {
+      return super.restIncrement();
+    } finally {
+      _$_PomodoroStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void restDecrement() {
+    final _$actionInfo = _$_PomodoroStoreActionController.startAction(
+        name: '_PomodoroStore.restDecrement');
+    try {
+      return super.restDecrement();
+    } finally {
+      _$_PomodoroStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+started: ${started},
+minutes: ${minutes},
+seconds: ${seconds},
+workTime: ${workTime},
+restTime: ${restTime},
+intervalType: ${intervalType}
     ''';
   }
 }
