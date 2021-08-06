f6a9d92955e9b84d765188f60334f44d4230d0f0
Vanderllee <vanderllee@gmail.com>
2021-08-06 18:16:43 -0300

    changes props components

--- vdee_pomodoro/lib/components/EntradaTempo.dart
+++ vdee_pomodoro/lib/components/EntradaTempo.dart
@@ -1,32 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../store/pomodoro.store.dart';
 
 class EntradaTempo extends StatelessWidget {
+  final String title;
+  final int value;
 
- final String title;
- final int value;
+  //functions params
+  final void Function()? inc;
+  final void Function()? dec;
 
   const EntradaTempo({
     Key? key,
+
     required this.title,
-    required  this.value,
+    required this.value,
+     this.inc,
+     this.dec,
+
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
     return Column(
       children: [
         Text(
           this.title,
           style: TextStyle(
-          fontSize: 25,
+            fontSize: 25,
           ),
         ),
-        SizedBox(height: 15,),
+        SizedBox(
+          height: 15,
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {}, 
+              onPressed: this.dec,
               child: Icon(
                 Icons.arrow_downward,
                 color: Colors.white,
@@ -34,19 +47,17 @@ class EntradaTempo extends StatelessWidget {
               style: ElevatedButton.styleFrom(
                 shape: CircleBorder(),
                 padding: EdgeInsets.all(15),
-                primary: Colors.red,
+                primary: store.isWorking() ? Colors.red : Colors.green,
               ),
             ),
-
             Text(
               '${this.value} min',
               style: TextStyle(
                 fontSize: 18,
               ),
-              ),
-
-             ElevatedButton(
-              onPressed: () {}, 
+            ),
+            ElevatedButton(
+              onPressed: this.inc,
               child: Icon(
                 Icons.arrow_upward,
                 color: Colors.white,
@@ -54,7 +65,7 @@ class EntradaTempo extends StatelessWidget {
               style: ElevatedButton.styleFrom(
                 shape: CircleBorder(),
                 padding: EdgeInsets.all(15),
-                primary: Colors.red,
+                primary: store.isWorking() ? Colors.red : Colors.green,
               ),
             ),
           ],
